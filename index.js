7b0dca52976f27785306722d404598df77f73367
kkufieta <katharina.kufieta@gmail.com>
2020-09-12 11:51:17 -0400

    Restructure code to allow for generic reducer function

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// Initialize state to an empty array, because the first time the
-// function is called, the state is undefined.
-function todos(state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo]);
-  }
+// ---- Library Code (State Management Library) ----
+// This is code that you'd download from NPM.
 
-  return state;
-}
 // A function that mimics the Redux store behavior.
 // Implemented to
-function createStore() {
+function createStore(reducer) {
   // The store should have 4 parts:
   // 1. The state
   // 2. Get the state
@@ -28,17 +22,33 @@ function createStore() {
   };
 
   const dispatch = (action) => {
-    state = todos(state, action);
+    state = reducer(state, action);
     listeners.forEach(listener => listener());
   };
 
   return {getState, subscribe, dispatch};
 }
 
-const store = createStore();
+const store = createStore(todos);
 
 const unsubscribe = store.subscribe(() => {
   console.log('The state changed to: ', state);
 })
 
 const unsubscribe = store.subscribe(() => {console.log('The store changed.')})
+
+
+// ---- App Code ----
+// This is code a user would implement to define changes to the
+// state based on actions.
+
+// Reducer function
+// Initialize state to an empty array, because the first time the
+// function is called, the state is undefined.
+function todos(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo]);
+  }
+
+  return state;
+}
