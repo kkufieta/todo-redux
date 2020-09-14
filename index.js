15eb830c6a77f63741e81e3d2e40d1ab4880dc0c
kkufieta <katharina.kufieta@gmail.com>
2020-09-14 07:54:48 -0400

    Add Thunk + separate addTodo UI vs. data fetching
    Adding Thunk middleware allows us to create handlers
    that handle callback functions in addition to objects returned
    by action creators.
    
    This helps in separating UI logic that should be reserved
    to components from data handling logic.

--- index.js
+++ index.js
@@ -40,6 +40,26 @@ function receiveDataAction(todos, goals) {
   }
 }
 
+// Thunk action creator handlers
+// These functions make asynchronous API calls and
+// are handled using redux-thunk, which is middleware that
+// checks whether or not we return an object or function
+// and handles it appropriately.
+// For an example for a thunk function, see the middleware portion
+// below.
+function handleAddTodo(name) {
+  return (dispatch) => {
+    API.saveTodo(name)
+        .then((todo) => {
+          dispatch(addTodoAction(todo));
+        })
+        .catch(() => {
+          alert('An error occurred. Try again.');
+        })
+  };
+}
+
+
 // ---- REDUCERS ----
 // Reducer function for Todos
 // Initialize state to an empty array, because the first time the
@@ -92,29 +112,43 @@ function loading(state = true, action) {
 }
 
 // ---- MIDDLEWARE ----
-const checker =
-    (store) => (next) => (action) => {
-      if ((action.type === ADD_TODO &&
-           action.todo.name.toLowerCase().includes('bitcoin')) ||
-          (action.type === ADD_GOAL &&
-           action.goal.name.toLowerCase().includes('bitcoin'))) {
-        return alert('Nope. That\'s a bad idea.');
-      }
+const checker = (store) => (next) => (action) => {
+  if ((action.type === ADD_TODO &&
+       action.todo.name.toLowerCase().includes('bitcoin')) ||
+      (action.type === ADD_GOAL &&
+       action.goal.name.toLowerCase().includes('bitcoin'))) {
+    return alert('Nope. That\'s a bad idea.');
+  }
 
-      return next(action);
-    }
+  return next(action);
+};
 
-const logger =
-    (store) => (next) => (action) => {
-      console.group(action.type);
-      console.log('Action: ', action);
-      const result = next(action);
-      console.log('New state: ', store.getState());
-      console.groupEnd();
-      return result;
-    }
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log('Action: ', action);
+  const result = next(action);
+  console.log('New state: ', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+
+// This function shows the idea of how a Thunk is implemented.
+// Because this is such a common pattern, there is a library
+// redux-thunk that implements this for us. That's what is used
+// in the middleware, instead of this function.
+/*
+const thunk = (store) => (next) => (action) => {
+  if (typeof action === 'function') {
+    return action(store.dispatch);
+  }
+
+  return next(action);
+};
+*/
+
 
 // ---- STORE ----
 const store = Redux.createStore(
     Redux.combineReducers({todos, goals, loading}),
-    Redux.applyMiddleware(checker, logger));
+    Redux.applyMiddleware(ReduxThunk.default, checker, logger));
